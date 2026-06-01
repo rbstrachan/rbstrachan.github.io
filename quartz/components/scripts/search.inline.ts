90a917adf37d5d779a68ac773bcf5b1e933fda85
Ross Strachan <8963519+rbstrachan@users.noreply.github.com>
2026-06-01 00:46:34 -0400

    add `/` key to be used a search trigger

--- quartz/components/scripts/search.inline.ts
+++ quartz/components/scripts/search.inline.ts
@@ -242,7 +242,7 @@ async function setupSearch(searchElement: Element, currentSlug: FullSlug, data: 
 
   let currentHover: HTMLInputElement | null = null
   async function shortcutHandler(e: HTMLElementEventMap["keydown"]) {
-    if (e.key === "k" && (e.ctrlKey || e.metaKey) && !e.shiftKey) {
+    if ((e.key === "k" && (e.ctrlKey || e.metaKey) && !e.shiftKey) || (e.key === "/" && !e.shiftKey)) {
       e.preventDefault()
       const searchBarOpen = container.classList.contains("active")
       searchBarOpen ? hideSearch() : showSearch("basic")
