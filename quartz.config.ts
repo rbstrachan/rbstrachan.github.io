e85fcb23cd9aa532205ea20b518c403b25955f72
Ross Strachan <8963519+rbstrachan@users.noreply.github.com>
2026-06-03 23:39:09 -0400

    minor edits

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,12 +1,6 @@
 import { QuartzConfig } from "./quartz/cfg"
 import * as Plugin from "./quartz/plugins"
 
-/**
- * Quartz 4 Configuration
- *
- * See https://quartz.jzhao.xyz/configuration for more information.
- */
-
 const lang = process.env.QUARTZ_LANG || 'en';
 const base = "reiwa.ca"
 
@@ -90,7 +84,6 @@ const config: QuartzConfig = {
       Plugin.Static(),
       Plugin.Favicon(),
       Plugin.NotFoundPage(),
-      // Comment out CustomOgImages to speed up build time
       Plugin.CustomOgImages(),
     ],
   },
