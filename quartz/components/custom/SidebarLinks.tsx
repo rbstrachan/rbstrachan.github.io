5de2f28fa1b1e94bc3ae828ce15b3f9eabf6754a
Ross Strachan <8963519+rbstrachan@users.noreply.github.com>
2026-08-07 23:10:00 -0400

    update sidebarLinks custom component

--- quartz/components/custom/SidebarLinks.tsx
+++ quartz/components/custom/SidebarLinks.tsx
@@ -1,15 +1,15 @@
 import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
-function SidebarLinks({ displayClass }: QuartzComponentProps) {
+function SidebarLinks({ fileData, displayClass }: QuartzComponentProps) {
   return (
     <div class={`sidebar-links ${displayClass ?? ""}`}>
-      {/* <a href="/about">About Me</a> */}
-      <span><a href="/cv">CV</a><small><b> ・ </b></small><a href="/qr">QR</a></span>
+      {fileData?.slug && fileData.slug !== "index" && <a href="/">About Me</a>}
+      <span><a href="/cv">CV</a><small><b> ・ </b></small><a href="/qr">QR</a></span>
       <a href="/tutoring/lessons">Language Tutoring</a>
       <a href="/guestbook">Guestbook</a>
     </div>
   )
 }
 
-export default (() => SidebarLinks) satisfies QuartzComponentConstructor
 // <a href="/shop">Shop</a>
+export default (() => SidebarLinks) satisfies QuartzComponentConstructor
